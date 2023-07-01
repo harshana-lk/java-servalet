1a68b47b6e3f892c4ad1f8fa1c05f8d1837004ed
Harshana Jayaweera <mailtoharshanaonlive@gmail.com>
2023-07-01 17:22:45 +0530

    added init method

--- src/main/java/me/harshu/javaservalet/Customer.java
+++ src/main/java/me/harshu/javaservalet/Customer.java
@@ -7,14 +7,18 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-public class TestTask extends HttpServlet {
+public class Customer extends HttpServlet{
+
+    @Override
     public void init() throws ServletException {
         String name = getServletConfig().getInitParameter("name");
         String city = getServletConfig().getInitParameter("city");
         System.out.println(name + ": " + city);
     }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("HI Server");
+//        super.doGet(req, resp);
+        System.out.println("Customer Created");
     }
 }
